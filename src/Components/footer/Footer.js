b0d573ea7fadbffd5c5824d363d0c4f6abad3f34
omkar213 <omkardhumal213@gmail.com>
2023-04-14 16:40:29 +0530

    minor changes fixed

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,14 +1,67 @@
-import React from 'react'
-import styles from './Footer.module.scss';
+import React from "react";
+import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import {BsInstagram} from 'react-icons/bs'
+import './footer.css'
 
 const date = new Date();
 const year = date.getFullYear();
 
+const logo = (
+  <div className="logo">
+    <Link to="/">
+      <h2>
+        e<span className="half-log">BoOk</span>
+      </h2>
+    </Link>
+  </div>
+);
+
+const twitter = "https://twitter.com/login?lang=en";
+const facebook = "https://www.facebook.com/";
+const instagram = "https://www.instagram.com/";
 
 const Footer = () => {
+  
   return (
-    <div className={styles.footer}>&copy; {year} All Rights Reserved</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-section">{logo}</div>
+      <div className="footer-section">
+        <h3 className="footer-title">Company</h3>
+        <br />
+        <Link to="/about" className="footer-links">
+          About Us
+        </Link>
+        <br />
+        <Link to="/contact" className="footer-links">
+          Contact Us
+        </Link>
+      </div>
+      <div className="footer-section">
+        <h3 className="footer-title">Social</h3>
+        <br />
+        <Link to={twitter} className="footer-links">
+        <FaTwitter/> Twitter
+        </Link>
+        <br />
+        <Link to={facebook} className="footer-links">
+          <FaFacebookF/> FaceBook
+        </Link>
+      </div>
+      <div className="footer-section">
+        <h3 className="footer-title">Help</h3>
+        <br />
+        <Link to="/contact" className="footer-links">
+          Contact Us
+        </Link>
+        <br />
+        <Link to="/about" className="footer-links">
+          About Us
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Pages/auth/Register.js
+++ src/Pages/auth/Register.js
@@ -3,7 +3,12 @@ import styles from "./auth.module.scss";
 import registerImg from "../../Assets/register.png";
 import Card from "../../Components/card/Card";
 import { Link, useNavigate } from "react-router-dom";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithPopup,
+} from "firebase/auth";
 import Loader from "../../Components/loader/Loader";
 import { toast } from "react-toastify";
 import { auth } from "../../Firebase/config";
@@ -17,34 +22,72 @@ const Register = () => {
   const [cPassword, setCPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const validateEmail = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const validatePassword = () => {
+    return password.length >= 6;
+  };
+
+  const validateCPassword = () => {
+    return cPassword === password && validatePassword();
+  };
+
+  const inputClass = (isValid, value) => {
+    if (value === "") {
+      return "";
+    }
+    return isValid ? styles.valid : styles.invalid;
+  };
+
+  const inputCPassword = (isValid, value, isMatching) => {
+    if (value === "") {
+      return "";
+    }
+    return isValid && isMatching ? styles.valid : styles.invalid;
+  };
+
   const navigate = useNavigate();
 
   const registerUser = (e) => {
     e.preventDefault();
     if (password !== cPassword) {
       toast.error("Passwords Do Not Match 😔");
-    }
-    setIsLoading(true);
+    } else {
+      setIsLoading(true);
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        setIsLoading(false);
-        toast.success("Registration successfully 😀");
-        const userDocRef = addDoc(collection(db, "users"), {
-          email: user.email,
-          uid: user.uid,
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          setIsLoading(false);
+          toast.success(
+            "Registration successfully! Pls go to your email and click on the link to verify your email!!"
+          );
+          sendEmailVerification(user)
+            .then(() => {
+              // Email verification sent
+              console.log("Email verification sent");
+            })
+            .catch((error) => {
+              // Error sending email verification
+              console.log(error.message);
+            });
+          const userDocRef = addDoc(collection(db, "users"), {
+            email: user.email,
+            uid: user.uid,
+          });
+          // console.log("New user added with ID: ", userDocRef.id);
+          navigate("/login");
+        })
+        .catch((error) => {
+          toast.error(error.message);
+          setIsLoading(false);
         });
-        // console.log("New user added with ID: ", userDocRef.id);
-        navigate("/");
-
-      })
-      .catch((error) => {
-        toast.error(error.message);
-        setIsLoading(false)
-      });
+    }
   };
 
   const provider = new GoogleAuthProvider();
@@ -59,7 +102,7 @@ const Register = () => {
         console.log("New user added with ID: ", userDocRef.id);
         setIsLoading(false);
         toast.success("Login Successfully");
-        navigate("/")
+        navigate("/");
       })
       .catch((error) => {
         toast.error(error.message);
@@ -69,7 +112,7 @@ const Register = () => {
 
   return (
     <>
-      {isLoading && <Loader/>}
+      {isLoading && <Loader />}
       <section className={`container ${styles.auth}`}>
         <Card>
           <div className={styles.form}>
@@ -82,6 +125,7 @@ const Register = () => {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                className={inputClass(validateEmail(), email)}
               />
               <input
                 type="password"
@@ -89,6 +133,7 @@ const Register = () => {
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                className={inputClass(validatePassword(), password)}
               />
               <input
                 type="password"
@@ -96,13 +141,21 @@ const Register = () => {
                 required
                 value={cPassword}
                 onChange={(e) => setCPassword(e.target.value)}
+                className={inputCPassword(
+                  validateCPassword(),
+                  cPassword,
+                  cPassword === password
+                )}
               />
               <button type="submit" className="--btn --btn-primary --btn-block">
                 Register
               </button>
             </form>
-            <br/>
-            <button className="--btn --btn-danger --btn-block" onClick={signInWithGoogle}>
+            <br />
+            <button
+              className="--btn --btn-danger --btn-block"
+              onClick={signInWithGoogle}
+            >
               <FaGoogle color="#fff" /> Login With Google
             </button>
             <span className={styles.register}>
